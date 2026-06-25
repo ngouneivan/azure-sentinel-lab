d9d496d2a7bb9c7713fded99312908e1daa58a8d
ngouneivan <ivantonfack53@gmail.com>
2026-06-25 18:05:05 +0200

    feat: backend Terraform remote state — stsentinelstate4593

---
+++ infrastructure/backend.tf
@@ -0,0 +1,10 @@
+﻿# backend.tf — Remote state Azure Blob
+# A compléter après création du storage account
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-tfstate-sentinel"
+    storage_account_name = "stsentinelstate4593"
+    container_name       = "tfstate"
+    key                  = "sentinel-lab.tfstate"
+  }
+}
